52963f246070099b5faf3917ab898013738aa276
Ivan-Klabucar <ivan.klabucar@fer.hr>
2021-05-11 13:44:57 +0200

    added comments

--- include/biosoup/nucleic_acid.hpp
+++ include/biosoup/nucleic_acid.hpp
@@ -92,29 +92,29 @@ class NucleicAcid {
     
     deflated_quality.reserve(quality_len / 32. + .999);
     quality_levels.reserve(quality_len / 512. + .999);
-    for (std::uint32_t i = 0; i < quality_len; i += 512) {
-      std::uint32_t index_limit = std::min(i + 512, quality_len);
+    for (std::uint32_t i = 0; i < quality_len; i += 512) {         // for each window of 512 find the corresponding encoding and apply it
+      std::uint32_t index_limit = std::min(i + 512, quality_len);  // upper bound of the window
       std::uint64_t quality_sum = 0;
-      std::vector<std::int32_t> quality_freq(100, 0);
+      std::vector<std::int32_t> quality_freq(100, 0);              // counting sort of quality values
       for (std::uint32_t j = i; j < index_limit; ++j) {
         std::uint8_t curr_quality = quality[j] - '!';
         quality_freq[curr_quality]++;
         quality_sum += curr_quality;
       }
-      std::vector<uint8_t> curr_levels;
+      std::vector<uint8_t> curr_levels;                            // current encoding values
       curr_levels.reserve(4);
       DecideQualityLevels(quality_freq, curr_levels, quality_sum, index_limit, i);
       //Encoding quality values of current window
       std::uint64_t block = 0;
-      std::vector<std::int32_t> diffs;
+      std::vector<std::int32_t> diffs;                             // vector of distances to each encoding
       diffs.reserve(4);
       for (std::uint32_t j = i; j < index_limit; ++j) {
         diffs.clear();
         std::uint8_t curr_quality = quality[j] - '!';
-        for (auto x : curr_levels) diffs.emplace_back(std::abs(x - curr_quality));
-        std::uint64_t c = std::min_element(diffs.begin(), diffs.end()) - diffs.begin();
-        block |= c << ((j << 1) & 63);
-        if ( j == quality_len - 1 || ((j + 1) & 31) == 0 ) {
+        for (auto x : curr_levels) diffs.emplace_back(std::abs(x - curr_quality));      // calculate distance from each encoding of current quality score 
+        std::uint64_t c = std::min_element(diffs.begin(), diffs.end()) - diffs.begin(); // use the encoding closest to actual quality score
+        block |= c << ((j << 1) & 63);                                                  // append current encoding to block
+        if ( j == quality_len - 1 || ((j + 1) & 31) == 0 ) {                            // if block is full append it to deflated_quality
           deflated_quality.emplace_back(block);
           block = 0;
         }
@@ -158,19 +158,19 @@ class NucleicAcid {
     if (quarter < 1.0) quarter = 1.0;
     std::int32_t num_of_upper_levels;
     std::int32_t num_of_lower_levels;
-    if (mod_q > avg_q) {
+    if (mod_q > avg_q) {              // if distribution is negatively skewed
       num_of_upper_levels = static_cast<std::int32_t>((max_q - mod_q) / quarter);
       num_of_lower_levels = 4 - num_of_upper_levels - 1;
-    } else if (mod_q < avg_q) {
+    } else if (mod_q < avg_q) {      // if distribution is positively skewed
       num_of_lower_levels = static_cast<std::int32_t>((mod_q - min_q) / quarter);
       num_of_upper_levels = 4 - num_of_lower_levels - 1;
-    } else {
-      num_of_lower_levels = 1;
+    } else {                         // if distribution is not skewed
+      num_of_lower_levels = 1;       // the mode value will be considered a lower level
       num_of_upper_levels = 2;
     }
     std::int32_t upper_step = (max_q - mod_q) / (num_of_upper_levels + 1);
     std::int32_t lower_step = (mod_q - min_q) / (num_of_lower_levels + 1);
-    std::uint32_t discretization_levels = 0;
+    std::uint32_t discretization_levels = 0;  // to be appended to quality levels
     std::uint32_t curr_level = min_q;
     auto process_level = [&curr_levels, &discretization_levels] (std::uint32_t level) {
       curr_levels.emplace_back(static_cast<uint8_t>(level));
@@ -187,9 +187,9 @@ class NucleicAcid {
       curr_level += upper_step;
       process_level(curr_level);
     }
-    quality_levels.push_back(discretization_levels);
-    std::reverse(curr_levels.begin(), curr_levels.end());
-  }
+    quality_levels.push_back(discretization_levels);       // append encoding for current window to quality_levels vector
+    std::reverse(curr_levels.begin(), curr_levels.end());  // index of encoding in curr_levels must be proportional to 
+  }                                                        // how much discretization_levels have to be shifted to the right to get the same encoding
 
   std::uint64_t Code(std::uint32_t i) const {
     std::uint64_t x = 0;
@@ -204,7 +204,7 @@ class NucleicAcid {
     if (is_reverse_complement) {
       i = inflated_len - i - 1;
     }
-    std::int32_t index = (deflated_quality[i >> 5] >> ((i << 1) & 63)) & 3;
+    std::int32_t index = (deflated_quality[i >> 5] >> ((i << 1) & 63)) & 3; // offset of the decoding
     return static_cast<std::uint8_t>((quality_levels[i >> 9] >> (index * 8)) & 255);
   }
 
@@ -245,9 +245,9 @@ class NucleicAcid {
   std::uint32_t id;  // (optional) initialize num_objects to 0
   std::string name;
   std::vector<std::uint64_t> deflated_data;
-  std::vector<std::uint64_t> deflated_quality;
+  std::vector<std::uint64_t> deflated_quality; // each Phred quality score encoded with 2 bits
   std::vector<std::uint8_t> block_quality;  // (optional) Phred quality scores
-  std::vector<std::uint32_t> quality_levels;
+  std::vector<std::uint32_t> quality_levels; // 2 bit decodings for each window of 512 quality scores
   std::uint32_t inflated_len;
   bool is_reverse_complement;
 };
